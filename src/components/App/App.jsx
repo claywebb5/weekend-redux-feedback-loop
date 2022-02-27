92ea9fca41544b4944b0807b1fc92f1cf200af54
Clay Webb <clay_webb@outlook.com>
2022-02-26 21:31:59 -0600

    Created reducer for all the feedback components; added descriptive code comments to understand the feedbackReducer; added feedbackReducer to store

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,10 +6,7 @@ function App() {
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+//====<Reducer>=========================================================================
+  // Create one feedback reducer to hold all the action types for the app
+  const feedbackReducer = (state = [], action) => { // Make state an object to be able to hold the different action.payloads
+    // Feeling action
+    if (action.type === 'FEELING'){
+      state = {...state, feeling: action.payload} // Spread state and set the `components` property equal to it's action.payload
+    }
+    // Understanding action
+      if (action.type === 'UNDERSTANDING'){
+        state = {...state, understanding: action.payload} // Spread state and set the `components` property equal to it's action.payload
+      }
+    // Support action
+    if (action.type === 'SUPPORT'){
+      state = {...state, support: action.payload} // Spread state and set the `components` property equal to it's action.payload
+    }
+    // Comments action
+    if (action.type === 'COMMENTS'){
+      state = {...state, comments: action.payload} // Spread state and set the `components` property equal to it's action.payload
+    }
+    return state; // Return the state object, able to useSelector to render the specific properties on the Review component
+  };
+
+
+//====<Store>=========================================================================
+const storeInstance = createStore(
+
+    combineReducers({
+        feedbackReducer,
+    }),
+    // Tell redux that we want to use our new logger
+    applyMiddleware(
+      logger 
+    )
+  );
+
+
+//====<Provider lets redux and react talk to each other>=============================
+ReactDOM.render(
+    <Provider store={storeInstance}>
+        <App /> 
+    </Provider>,
+    document.getElementById('root')
+);
+
+
+
 registerServiceWorker();
